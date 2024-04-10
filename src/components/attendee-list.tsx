4371e30cf0f7fd8d6d050c898abeb8920e207ce3
ecthon <ecthon@gmail.com>
2024-04-09 23:09:52 -0400

    feature: save current page with window.history

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -22,8 +22,14 @@ interface IAttendee {
 
 export function AttendeeList() {
   const [search, setSearch] = useState('')
-  // const [page, setPage] = useState(1)
-  const page = 1
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
 
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<IAttendee[]>([])
@@ -49,9 +55,11 @@ export function AttendeeList() {
   function setCurrentPage(page: number) {
     const url = new URL(window.location.toString())
     
-    url.searchParams.set('page', String(page + 1))
+    url.searchParams.set('page', String(page))
 
     window.history.pushState({}, "", url)
+
+    setPage(page)
   }
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
@@ -61,18 +69,22 @@ export function AttendeeList() {
 
   function goToFirstPage() {
     // setPage(1)
+    setCurrentPage(1)
   }
 
   function goToPreviousPage() {
     // setPage (page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToNextPage() {
     // setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToLastPage() {
     // setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   return (
